a6f0d756cb00f3c4855b2de9c1542738a3e9e360
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-10-06 06:59:10 -0300

    Reset html
    Video time: 00:12:50

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/*==================== SHOW MENU ====================*/
+
+
+/*===== MENU SHOW =====*/
+
+
+/*===== MENU HIDDEN =====*/
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+
+/*==================== SWIPER DISCOVER ====================*/
+
+
+/*==================== VIDEO ====================*/
+
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+
+// Activate / deactivate the theme manually with the button
